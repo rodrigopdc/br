38e6dc1f5fc260b26ba7e28eb48cb38cb630d5e6
rodrigopdc <34666519+rodrigopdc@users.noreply.github.com>
2017-12-19 18:33:08 -0200

    Add files via upload

---
+++ material-design-transformation.markdown
@@ -0,0 +1,7 @@
+Material Design - Transformation
+--------------------------------
+
+
+A [Pen](https://codepen.io/ehellman/pen/BLyoRE) by [Erik Hellman](https://codepen.io/ehellman) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/ehellman/pen/BLyoRE/license).
--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-var email = document.getElementById('email'),
-    button = document.getElementById('button');
-
-function validateEmail(email) {
-    var ex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    return ex.test(email);
-}
-
-email.addEventListener('keydown', function() {
-  var email = this.value;
+$(function() {
+  $('.wrapper').click(function() {
+  	$('.content').toggleClass('open');
+  	$(this).toggleClass('open');
+  })
   
-  if(validateEmail(email)) {
-    button.classList.add('is-active');
-  }
-});
-
-button.addEventListener('click', function(e){
-  e.preventDefault();
-  this.classList.add('is-done','is-active');
-  
-  setTimeout(function(){ 
-    button.innerHTML = "Thanks! Check Your Email."
-  }, 500);
-});
-
-
-
-
-
-
+})
